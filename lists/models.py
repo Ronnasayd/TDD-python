a4b4e67f5ddafe5f6095f140229c9ba3be110273
Ronnasayd Machado <ronnasayd@hotmail.com>
2019-08-04 00:01:50 -0300

    Implement duplicate item validation at model layer

--- lists/models.py
+++ lists/models.py
@@ -9,8 +9,12 @@ class List(models.Model):
 
 
 class Item(models.Model):
-    text = models.TextField(default='',)
+    text = models.TextField(default='')
     list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
 
+    class Meta:
+        ordering = ('id',)
+        unique_together = ('list', 'text')
+
     def __str__(self):
-        return "text: {}".format(self.text)
+        return self.text
